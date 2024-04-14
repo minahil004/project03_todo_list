f3ed50f11cb1883d7c6abdde02d3381428ec9b3f
KashfUlHudaa <166825328+minahil004@users.noreply.github.com>
2024-04-14 09:25:34 +0500

    First commit of todo list

---
+++ index.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let TodoList = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "TodoList",
+            type: "input",
+            message: "What you want to add in your Todos list?"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more?",
+            default: "false"
+        }
+    ]);
+    TodoList.push(addTask.TodoList);
+    condition = addTask.addMore;
+    console.log(TodoList);
+}
